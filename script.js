08deaaee6a62ffbadf0dd3452a1b135bdddc5278
Christian <christianyip92@gmail.com>
2022-07-06 15:16:47 -0700

    Add scoring system and final result.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let compScore = 0;
+
 function playRound(){
     function computerPlay() {
         const choices = ["Rock", "Paper", "Scissors"];
@@ -19,22 +22,28 @@ function playRound(){
     // }
     
     if (playerChoice == "rock" && compChoice == "Scissors") {
+        playerScore += 1; 
         return "Computer chose Scissors. Rock beats Scissors. YOU WIN!";
     } else if (playerChoice == "rock"  && compChoice == "Paper") {
+        compScore += 1;
         return "Computer chose Paper. Paper beats Rock. YOU LOSE!";
     } else if (playerChoice == "rock" && compChoice == "Rock") {
         return "Computer chose Rock. It's a TIE!";
    
     } else if (playerChoice == "paper" && compChoice == "Rock") {
+        playerScore += 1; 
         return "Computer chose Rock, YOU WIN!";
     } else if (playerChoice == "paper" && compChoice == "Paper") {
         return "Computer chose Paper. It's a TIE!"
     } else if (playerChoice == "paper" && compChoice == "Scissors") {
+        compScore += 1;
         return "Computer chose Scissors. Scissors beats Paper. YOU LOSE!"
     
     } else if (playerChoice == "scissors" && compChoice == "Paper") {
+        playerScore += 1; 
         return "Computer chose Paper. Scissors beats Paper. YOU WIN!"
     } else if (playerChoice == "scissors" && compChoice == "Rock") {
+        compScore += 1;
         return "Computer chose Rock. Rock beats Scissors. YOU LOSE!"
     } else if (playerChoice == "scissors" && compChoice == "Scissors") {
         return "Computer chose Scissors. It's a TIE!"
@@ -42,26 +51,24 @@ function playRound(){
 }
 
 function game() { 
-    let loop = function loop() {
+    function loop() {
         for (i = 0; i < 5; i++) {
             console.log("Round " + (i + 1));
             console.log(playRound());
-            score();
             console.log("Player Score: " + playerScore + " | " + "Computer Score: " + compScore);
         }
     }
-
-    let playerScore = 0;
-    let compScore = 0;
-
-    function score() {
-        if (loop == "Computer chose Scissors. Rock beats Scissors. YOU WIN!") {
-            playerScore += 1; 
+    
+    function result() {
+        if (playerScore > compScore) {
+            console.log("FINAL RESULT: YOU WIN!");
+        } else if (playerScore < compScore) {
+            console.log("FINAL RESULT: YOU LOSE!");
+        } else if (playerScore == compScore) {
+            console.log("FINAL RESULT: IT'S A TIE. BOOOOOO!")
         }
     }
-    
-    
-    return loop(), score();
+    return loop(), result();
 }
 
 console.log(game());
