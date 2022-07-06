b87fbb8ebef222e363fc1363ec1270b17edce61d
Christian <christianyip92@gmail.com>
2022-07-06 11:07:44 -0700

    Start feature that returns error if invalid user input.

--- script.js
+++ script.js
@@ -1,41 +1,50 @@
 
 function game() {
-    function computerPlay() {
-        const choices = ["Rock", "Paper", "Scissors"];
-        compChoice = choices[Math.floor(Math.random() * choices.length)];
-        return compChoice;
-    }
-    computerPlay();
-    // console.log(compChoice);
+    function playRound(){
+        function computerPlay() {
+            const choices = ["Rock", "Paper", "Scissors"];
+            compChoice = choices[Math.floor(Math.random() * choices.length)];
+            return compChoice;
+        }
+        computerPlay();
+        // console.log(compChoice);
 
-    let playerChoice = prompt("Please choose Rock, Paper, or Scissors.").toLowerCase();
-    
-    if (playerChoice == "rock" && compChoice == "Scissors") {
-        // console.log(compChoice);
-        return "Computer chose Scissors. Rock beats Scissors. YOU WIN!"
-    } else if (playerChoice == "rock"  && compChoice == "Paper") {
-        // console.log(compChoice);
-        return "Computer chose Paper. Paper beats Rock. YOU LOSE!"
-    } else if (playerChoice == "rock" && compChoice == "Rock") {
-        // console.log(compChoice);
-        return "Computer chose Rock. It's a TIE!"
-    } else if (playerChoice == "paper" && compChoice == "Rock") {
-        return "Computer chose Rock, YOU WIN!"
-    
-    } else if (playerChoice == "paper" && compChoice == "Paper") {
-        return "Computer chose Paper. It's a TIE!"
-    } else if (playerChoice == "paper" && compChoice == "Rock") {
-        return "Computer chose Rock. Paper beats Rock. YOU WIN!"
-    } else if (playerChoice == "paper" && compChoice == "Scissors") {
-        return "Computer chose Scissors. Scissors beats Paper. YOU LOSE!"
-    
-    }else if (playerChoice == "scissors" && compChoice == "Paper") {
-        return "Computer chose Paper. Scissors beats Paper. YOU WIN!"
-    } else if (playerChoice == "scissors" && compChoice == "Rock") {
-        return "Computer chose Rock. Rock beats Scissors. YOU LOSE!"
-    } else if (playerChoice == "scissors" && compChoice == "Scissors") {
-        return "Computer chose Scissors. It's a TIE!"
+        let playerChoice = prompt("Please choose Rock, Paper, or Scissors.").toLowerCase();
+
+        // These do not work.
+        // if (playerChoice != "rock" || playerChoice != "paper" || playerChoice != "scissors") {
+        //     return playerChoice + " is not a valid input. Please enter either rock, paper, or scissors."
+        // }
+
+        // if (playerChoice = null) {
+        //     return playerChoice + " is not a valid input. Please enter either rock, paper, or scissors."
+        // }
+        
+        if (playerChoice == "rock" && compChoice == "Scissors") {
+            return "Computer chose Scissors. Rock beats Scissors. YOU WIN!"
+        } else if (playerChoice == "rock"  && compChoice == "Paper") {
+            return "Computer chose Paper. Paper beats Rock. YOU LOSE!"
+        } else if (playerChoice == "rock" && compChoice == "Rock") {
+            return "Computer chose Rock. It's a TIE!"
+        } else if (playerChoice == "paper" && compChoice == "Rock") {
+            return "Computer chose Rock, YOU WIN!"
+        
+        } else if (playerChoice == "paper" && compChoice == "Paper") {
+            return "Computer chose Paper. It's a TIE!"
+        } else if (playerChoice == "paper" && compChoice == "Rock") {
+            return "Computer chose Rock. Paper beats Rock. YOU WIN!"
+        } else if (playerChoice == "paper" && compChoice == "Scissors") {
+            return "Computer chose Scissors. Scissors beats Paper. YOU LOSE!"
+        
+        }else if (playerChoice == "scissors" && compChoice == "Paper") {
+            return "Computer chose Paper. Scissors beats Paper. YOU WIN!"
+        } else if (playerChoice == "scissors" && compChoice == "Rock") {
+            return "Computer chose Rock. Rock beats Scissors. YOU LOSE!"
+        } else if (playerChoice == "scissors" && compChoice == "Scissors") {
+            return "Computer chose Scissors. It's a TIE!"
+        }
     }
+    return playRound();
 }
 
 console.log(game());
